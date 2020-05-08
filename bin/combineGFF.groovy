e5261f2bf0f65feb8c28dd619dbcea2f0df4d5b8
Dave Roe <roex0050@umn.edu>
2020-05-07 19:36:36 -0500

    Some refactoring.

--- bin/combineGFF.groovy
+++ bin/combineGFF.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 /*
- * Combine multi-contig, multi-gene GFF files into one per contig.
+ * Combine multi-contig, multi-gene GFF into one per contig.
  *
  * @author Dave Roe
  * @todo could take a fasta file and only output the gff for that one
@@ -20,43 +20,22 @@ err = System.err
 
 @Field final OptionAccessor options = handleArgs(args)
 
-HashBasedTable<String, Integer, String> idPosTable = HashBasedTable.create()
+// overall goal is to consolidate gffs per id (description)
+// description (as in the fasta), position -> gff feature
+HashBasedTable<String, Integer, String> idPosGffTable = HashBasedTable.create()
 FileReader iReader = new FileReader(options.i)
-String desc = null
-Integer position = null
-String featureString = ""
-iReader.eachLine { l ->
-    if(l.startsWith("# Feature")) {
-        if(desc != null) {
-            if(debugging <= 3) {
-	            err.println "(${desc}, ${position}) = ${featureString}"
-            }
-            idPosTable.put(desc, position, featureString)
-            desc = null
-            position = null
-            featureString = ""
-        }
-        (fStr,id) = l.split('Feature:')
-        (desc, posTmp) = id.split('-')
-        position = posTmp.toInteger()
-        return
-    }
-    featureString += l + '\n'
-} // each input line
-if(debugging <= 3) {
-	err.println "(${desc}, ${position}) = ${featureString}"
-}
-idPosTable.put(desc, position, featureString)
 
-ArrayList<String> ids = idPosTable.rowKeySet()
-ArrayList<Integer> positions = idPosTable.columnKeySet().sort()
+loadGFF(iReader, idPosGffTable)
+
+ArrayList<String> ids = idPosGffTable.rowKeySet()
+ArrayList<Integer> positions = idPosGffTable.columnKeySet().sort()
 if(debugging <= 3) {
 	err.println "${ids.size()} ids, ${positions.size} positions"
 }
 ids.each { i ->
     String featuresFull = new String()
     positions.each { pos ->
-        featureTmp = idPosTable.get(i, pos)
+        featureTmp = idPosGffTable.get(i, pos)
         if((featureTmp != null) && (featureTmp.trim() != "")) {
             featuresFull += featureTmp
         }
@@ -65,6 +44,35 @@ ids.each { i ->
     writer.println featuresFull
     writer.close()
 } // each contig
+
+// load GFF into the table
+void loadGFF(iReader, idPosGffTable) {
+    String desc = null
+    Integer position = null
+    String featureString = ""
+    iReader.eachLine { l ->
+        if(l.startsWith("# Feature")) {
+            if(desc != null) {
+                if(debugging <= 3) {
+	                err.println "(${desc}, ${position}) = ${featureString}"
+                }
+                idPosGffTable.put(desc, position, featureString)
+                desc = null
+                position = null
+                featureString = ""
+            }
+            (fStr,id) = l.split('Feature:')
+            (desc, posTmp) = id.split('-')
+            position = posTmp.toInteger()
+            return
+        }
+        featureString += l + '\n'
+    } // each input line
+    if(debugging <= 3) {
+	    err.println "(${desc}, ${position}) = ${featureString}"
+    }
+    idPosGffTable.put(desc, position, featureString)
+} // loadGFF
 
 /*
  * handleArgs
@@ -78,7 +86,7 @@ ids.each { i ->
 OptionAccessor handleArgs(String[] args) { 
     CliBuilder cli = new CliBuilder(usage:'combineGFF.groovy [options] ', header:'Options:')
     cli.i(longOpt:'input', args:1, argName:'in', 'GFF input file', required: true)
-//    cli.f(longOpt:'fasta', args:1, argName:'fa', 'fasta input file', required: true)
+    //    cli.f(longOpt:'fasta', args:1, argName:'fa', 'fasta input file', required: true)
     cli.o(longOpt:'output', args:1, argName:'out', 'output directory', required: true)
     OptionAccessor options = cli.parse(args)
 
