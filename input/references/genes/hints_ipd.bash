7e76debc1d8783384b77865dc13bd1400721db55
Dave Roe <roex0050@umn.edu>
2020-12-29 08:52:26 -0600

    Removed. Not using augustus.

--- input/references/genes/hints_ipd.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for file in blat/*.psi
-do
-        # remove the file extension
-        name=$(echo $file | cut -f 1 -d '.')
-       echo "docker run -it --rm -v $(pwd):/inout chrishah/premaker-plus:18 blat2hints.pl  --in=/inout/"$file" --out=/inout/"$name".gff"
-#       `docker run -it --rm -v $(pwd):/inout chrishah/premaker-plus:18 blat2hints.pl  --in=/inout/"$file" --out=/inout/"$name".gff`
-done
--- input/references/genes/proteinprofile_ipd.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# for 2DP1, had to edit msa2prfl.pl and change die to warn on line 555
-for file in *_prot_msa.fasta
-do
-        # remove the file extension
-    name=$(echo $file | cut -f 1 -d '.')
-    echo "#!/bin/bash"
-    echo "$HOME/git/Augustus/scripts/msa2prfl.pl --qij $HOME/git/Augustus/config/profile/default.qij "$file" > "$name".prfl"
-done
